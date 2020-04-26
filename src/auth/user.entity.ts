638a830264af16aebdeeca4914fa52ba1e0b7cb3
Abhishekism9450 <abhishekism9450@gmail.com>
2020-04-26 14:34:53 +0530

    auth module created

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,10 @@
+import { BaseEntity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+export class User extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id:number;
+    @Column()
+    username:string;
+    @Column()
+    password:string;
+}
